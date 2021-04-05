3df37aff93257a7512a949b89063893f7c7fab2f
daivaxd <63538682+daivaxd@users.noreply.github.com>
2021-04-05 16:12:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-phone_number = "phone number"
-message = ["Good morning babe", "Good morning my love", "Good morning cutie"]
-scheduled_time = "08:00"
+phone_number = "+37060934266"
+message = ["Good morning babe"]
+scheduled_time = "17:00"
